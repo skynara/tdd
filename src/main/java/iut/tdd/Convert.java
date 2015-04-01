e96f2a047ef2c0ba26464f1b194760458a3ba64a
Samantha Ngoutane <samiecool@hotmail.fr>
2015-04-01 09:19:32 +0200

    Zero marche

--- src/main/java/iut/tdd/Convert.java
+++ src/main/java/iut/tdd/Convert.java
@@ -2,7 +2,7 @@ package iut.tdd;
 
 public class Convert {
 	public static String num2text(String input) {
-		return null;
+		return "zéro";
 	}
 	public static String text2num(String input) {
 		return null;
